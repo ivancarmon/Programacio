e5e71395d92c07560725e6f4b85f26bffdf5bcd4
Ivan <ivacarmon@alu.edu.gva.es>
2025-01-15 00:16:36 +0100

    commit

--- EjerciciosClase/src/matrius/Sudoku.java
+++ EjerciciosClase/src/matrius/Sudoku.java
@@ -4,80 +4,90 @@ import java.util.Scanner;
 
 public class Sudoku {
 
+
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num,x,y;
-		int[][] m = new int[9][9];
-		while(!comprobarVictoria(m)) {
-			System.out.println("Introduce el numero:");
-			num = sc.nextInt();
-			System.out.println("Introduce las cordenadas: ");
-			x = sc.nextInt();
-			y= sc.nextInt();
-			m = introducirNumeros(m,num,x,y);
-			ImprimirSudoku(m);
+		int x,y,num;
+		int[][] m = {{0,3,0,0,0,0,9,0,0},{0,0,6,0,0,0,0,0,0},{0,0,0,2,4,1,0,3,0},{0,0,0,9,0,0,7,0,0},{0,0,0,0,0,2,0,0,4},{0,8,0,0,7,0,0,2,0},{8,5,0,0,0,0,0,0,0},{0,9,0,7,0,4,0,0,0},{0,0,0,0,0,6,0,0,1}};
+		do {
+			ImrpimirSudoku(m);
 			
-		}
-		ImprimirSudoku(m);
-		System.out.println("Felicidades has completado el Sudoku");
+			System.out.println("introduce las cordenadas: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+			System.out.println("introduce el numero a insertar: ");
+			num = sc.nextInt();
+			insertarNumero(m, x, y, num);
+			
+		} while(!Numeros(m) || !comprobarVertical(m,y,num) || !comprobarHorizontal(m,x,num) || !comprobarCuadro(m,x,y,num) || !Numeros(m)) ;
+		ImrpimirSudoku(m);
+		System.out.println("¡Has ganado!");
 		sc.close();
 	}
-	private static int[][] introducirNumeros(int[][] m, int num,int x,int y) {
-		
-		if(comprobarVertical(num, m,x) && comprobarHorizontal(m) && comprobarCuadrado(m))
-		return m;
-	}
-	private static boolean comprobarVictoria(int[][] m) {
-		boolean Victoria = false,Numeros = true;
-		
+	private static boolean Numeros(int[][] m) {
+		boolean Numeros = true;
 		for (int i = 0; i < m.length && Numeros; i++) {
-			
+			for (int j = 0; j < m.length; j++) {
+				if (m[i][j] == 0) Numeros = false;
+			}
+		}
+		
+		return Numeros;
+	
+	}
+	public static void ImrpimirSudoku(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m.length; j++) {
 				
-				if(m[i][j] == 0) Numeros = false;
-				
-			}
-			
-		}
-		if(Numeros && comprobarVertical(m) && comprobarHorizontal(m) && comprobarCuadrado(m)) Victoria = true;
-		
-		return Victoria;
-	}
-	public static void ImprimirSudoku(int[][] m) {
-
-		for (int i = 0; i < m.length; i++) {
-
-			for (int j = 0; j < m[i].length; j++) {
-				if(m[i][j] !=0) {
-					System.out.print(m[i][j] + " ");
+				if (m[i][j] == 0) {
+					System.out.print("| ");
+					if(j == m.length - 1) System.out.print("|");
 				}
-			}
+				else if(j == m.length - 1) System.out.printf("|" + m[i][j] + "|");
+				else System.out.printf("|" + m[i][j]);
 
+			}
 			System.out.println();
 		}
+
+
 	}
-	public static boolean comprobarVertical(int[][] m,int num,int x,int y) {
+	public static boolean comprobarVertical(int[][] m,int y,int num) {
 		boolean Vertical = true;
-		int[] numeros = new int[9];
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
-				if(i == x) {
-					numeros[j] = m[i][j];
-					
-					
-				}
-				
-				
-			}
+		for (int i = 0; i < m.length && Vertical; i++) {
+			
+			if (m[i][y] == num) Vertical = false;
 			
 		}
-		
-		
-		
-		
 		
 		return Vertical;
 	}
+	public static int[][] insertarNumero(int[][] m, int x,int y,int num){
+		//if(!comprobarVertical(m,x,num)) System.out.println("Ya esta ese numero en esa columna");
+		if(comprobarVertical(m,y,num) && comprobarHorizontal(m,x,num)&& comprobarCuadro(m,x,y,num)) m[x][y] = num;
+		
+		
+		return m;
+	}
+	private static boolean comprobarHorizontal(int[][] m, int x, int num) {
+		boolean Horizontal = true;
+		for (int i = 0; i < m.length && Horizontal; i++) {
+			
+			if (m[x][i] == num) Horizontal = false;
+			
+		}
+		
+		return Horizontal;
+	}
+	private static boolean comprobarCuadro(int[][] m, int x, int y, int num) {
+		boolean cuadro = true;
+		
+		
+		
+		
+		return cuadro;
+	}
 
-
+	
 }
